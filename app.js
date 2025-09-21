a583699751aa1d85ded7e72d4fe9620de1c9573b
briannklk <briancaab587@gmail.com>
2025-09-21 20:45:08 -0300

    nuevo-challenge

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+
+// function para ingresar nombres
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.ariaValueMax.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre");
+        return;
+    
+    }
+}
+amigos.push(nombre);
+input.value = "";
+input.focus();
+actualizarLista();
+
