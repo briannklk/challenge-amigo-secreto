47cfff31efab00866d6514227591e6a675172d30
briannklk <briancaab587@gmail.com>
2025-09-21 21:33:48 -0300

    arreglo-de-function-parade-agregar-nombre

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
-// function para ingresar nombres
+// function de agregar nombres
 function agregarAmigo() {
-    const input = document.getElementById("amigo");
-    const nombre = input.ariaValueMax.trim();
+  const input = document.getElementById("amigo");
+  if (!input) { console.error("No existe amigo en el DOM"); return; }
 
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre");
-        return;
-    
-    }
+  const nombre = input.value.trim();
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre");
+    return;
+  }
+
+  amigos.push(nombre);
+  input.value = "";
+  input.focus();
+  actualizarLista();
+  console.log("amigos:", amigos);
 }
-amigos.push(nombre);
-input.value = "";
-input.focus();
-actualizarLista();
 
-//mostrar lista (lo mas dificil) 
-function actualizarLista() {
-    const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
-    for (let i = 0; i < amigos.length; i++) {
-        const li = document.getElementById("li");
-        li.textContent = amigos[i];
-        lista.appendChild(li);
-    }
-} 
-
-// sorteo del amigo secereto al azar
